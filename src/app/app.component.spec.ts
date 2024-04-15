4c73510f5fd4866412dd201e3a14203abf2fc460
daniele.gubbiotti <gubbiottidaniele@gmail.com>
2024-04-15 17:26:13 +0200

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-test-project' title`, () => {
+  it(`should have the 'reciphub' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-test-project');
+    expect(app.title).toEqual('reciphub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-test-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, reciphub');
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowseComponent } from './browse/browse.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'browse', component: BrowseComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/browse', pathMatch: 'full' } // Redirect to 'browse' by default
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
